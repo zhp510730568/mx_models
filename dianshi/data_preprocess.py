4cfc962b7e07fe2b9d4e84925fc6d383be482233
zhp510730568 <510730568@qq.com>
2018-05-06 15:22:59 +0800

    alexnet

--- dianshi/data_preprocess.py
+++ dianshi/data_preprocess.py
@@ -61,33 +61,9 @@ valid_data = loader(valid_ds, batch_size, shuffle=True, last_batch='keep', num_w
 # 交叉熵损失函数。
 softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 
-
-class Residual(nn.HybridBlock):
-    def __init__(self, channels, same_shape=True, **kwargs):
-        super(Residual, self).__init__(**kwargs)
-        self.same_shape = same_shape
-        with self.name_scope():
-            strides = 1 if same_shape else 2
-            self.conv1 = nn.Conv2D(channels, kernel_size=3, padding=1,
-                                   strides=strides)
-            self.bn1 = nn.BatchNorm()
-            self.conv2 = nn.Conv2D(channels, kernel_size=3, padding=1)
-            self.bn2 = nn.BatchNorm()
-            if not same_shape:
-                self.conv3 = nn.Conv2D(channels, kernel_size=1,
-                                       strides=strides)
-
-    def hybrid_forward(self, F, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        if not self.same_shape:
-            x = self.conv3(x)
-        return F.relu(out + x)
-
-
-class ResNet(nn.HybridBlock):
+class Alexnet(nn.HybridBlock):
     def __init__(self, num_classes, verbose=False, **kwargs):
-        super(ResNet, self).__init__(**kwargs)
+        super(Alexnet, self).__init__(**kwargs)
         self.verbose = verbose
         with self.name_scope():
             net = self.net = nn.HybridSequential()
@@ -129,7 +105,7 @@ class ResNet(nn.HybridBlock):
 
 def get_net(ctx):
     num_outputs = 100
-    net = ResNet(num_outputs)
+    net = Alexnet(num_outputs)
     net.initialize(ctx=ctx, init=init.Xavier())
     return net
 
